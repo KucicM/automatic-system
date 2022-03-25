eee7b541446a40db97d2d34b96a33d6d0b2fcf5d
kucicm <10778553+KucicM@users.noreply.github.com>
2022-03-25 19:19:05 +0100

    nuke

--- nuke-env.sh
+++ nuke-env.sh
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
-sudo apt purge -y htop
-sudo apt purge -y tmux
-sudo apt purge -y zsh
-sudo apt purge -y neovim
-sudo apt purge -y curl
-sudo apt purge -y lsof
-sudo apt purge -y i3
-sudo apt purge -y docker
-sudo apt purge -y docker-compose
+sudo apt purge -y htop tmux zsh neovim curl lsof i3 docker docker-compose
